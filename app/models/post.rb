5965a27ed5c84a3afac7ba93776352d3d6bb637c
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-08-31 17:08:05 -0400

    Complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :title_must_be_clickbait
+
+  def title_must_be_clickbait
+    unless title && title.match?(/Top \d+|Won't Believe|Secret|Guess/)
+      errors.add(:title, "Title does not adhere to the clickbait standard set.")
+    end
+  end
+
 end
